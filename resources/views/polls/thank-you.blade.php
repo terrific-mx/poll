9e98dd85de3d2f4b6138fc8986a0200f4322d32c
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-07-12 17:59:35 -0600

    Add thank you page for poll participation

--- resources/views/polls/thank-you.blade.php
+++ resources/views/polls/thank-you.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Thank You</title>
-</head>
-<body>
-    <h1>Thank you for your response!</h1>
-    <p>Your vote has been recorded.</p>
-</body>
-</html>
+<x-layouts.poll>
+    <div class="p-4 max-w-full md:p-6 md:max-w-sm mx-auto">
+        <div class="flex justify-center">
+            <flux:icon.check-circle variant="solid" class="text-green-500 size-12" />
+        </div>
+        <flux:heading size="lg" class="mt-6 text-center">Thank you for participating!</flux:heading>
+        <flux:text class="mt-1 text-center">Thanks for taking part in our poll. You may now close this window.</flux:text>
+    </div>
+</x-layouts.poll>
