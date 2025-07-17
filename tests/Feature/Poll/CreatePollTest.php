a2af3261a6c848df3659780e5a3ab126bcbbc351
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-07-16 18:45:33 -0600

    Add feature test for /polls/create route and import Pest actingAs helper

--- tests/Feature/Poll/CreatePollTest.php
+++ tests/Feature/Poll/CreatePollTest.php
@@ -1,4 +1,7 @@
+
 <?php
+
+use function Pest\Laravel\actingAs;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -77,5 +80,12 @@ describe('Poll Creation via Volt', function () {
 
         expect($poll->answers()->pluck('text')->toArray())
             ->toEqualCanonicalizing($answers);
+    });
+    
+    it('returns OK for /polls/create route as a logged in user', function () {
+        $user = User::factory()->create();
+        actingAs($user)
+            ->get('/polls/create')
+            ->assertOk();
     });
 });
