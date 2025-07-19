f55ecbf2bf01985d0ceb1aba2b7d6052dae9bee3
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-07-18 19:53:47 -0600

    Update poll response form layout and clean up CSS for Flux/Volt components

--- resources/views/livewire/polls/respond.blade.php
+++ resources/views/livewire/polls/respond.blade.php
@@ -35,18 +35,18 @@ new #[Layout('components.layouts.poll')] class extends Component {
     }
 }; ?>
 
-<div class="max-w-sm mx-auto py-6">
+<div class="max-w-sm mx-auto py-6 lg:p-10">
     @if ($showThankYou)
         <p>Thank you for your response</p>
     @else
-        <form wire:submit="submit">
+        <form wire:submit="submit" class="space-y-8">
             <flux:radio.group wire:model="answer_id" :label="$poll->question">
                 @foreach ($poll->answers as $answer)
                     <flux:radio value="{{ $answer->id }}" :label="$answer->text" />
                 @endforeach
             </flux:radio.group>
 
-            <div class="mt-4">
+            <div>
                 <flux:input
                     type="email"
                     wire:model="contact_email"
@@ -55,7 +55,7 @@ new #[Layout('components.layouts.poll')] class extends Component {
                 />
             </div>
 
-            <div class="mt-6">
+            <div>
                 <flux:button type="submit">Submit</flux:button>
             </div>
         </form>
