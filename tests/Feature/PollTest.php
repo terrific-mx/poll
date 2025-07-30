2fb1aa9583114be9a8f09f598f7ab52a8f6a08e5
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-07-29 18:57:02 -0600

    Add PollTest feature test file

---
+++ tests/Feature/PollTest.php
@@ -0,0 +1,6 @@
+<?php
+
+todo('create a poll with a name, a question and at least two poll options');
+todo('create an endpoint to view the poll like /p/{pollUuid}');
+todo('store visitor responses to the poll');
+todo('optionally store visitor email to the poll response');
